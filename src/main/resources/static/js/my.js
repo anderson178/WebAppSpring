b4d44d161fc5c25f6be9c0d32bc6e1800763d2b3
anderson178 <denismironenko@yandex.ru>
2019-05-26 13:09:22 +0300

    front-end - checkbox table

--- src/main/resources/static/js/my.js
+++ src/main/resources/static/js/my.js
@@ -15,16 +15,32 @@ $(document).ready(function () {
     //         });
     // });
 
-    $("#button").click(function () {
+    $("#fillButton").click(function () {
         $.get("http://127.0.0.1:8080/getAll")
             .done(function (data) {
                 $('#names tbody').empty();
                 for (var i = 0; i < data.length; i++) {
-                    $('#names tbody').append('<tr><td>' + data[i]['id'] + '</td><td>' + data[i]['firstName'] + '</td><td>' + data[i]['lastName'] + '</td><td>' + data[i]['middleName'] + '</td><td>' + data[i]['birthDate'] + '</td><td>' + data[i]['comment'] + '</td></tr>')
+                    var date = new Date(data[i]['birthDate']);
+                    // alert(date.getFullYear() + ' ' + date.getMonth()+' ' + date.getDate());
+                    $('#names tbody').append('<tr><td><input type="checkbox" id="blahA" value="1"/></td>   <td>' + data[i]['id'] + '</td><td>' + data[i]['firstName'] + '</td><td>' + data[i]['lastName'] + '</td><td>' + data[i]['middleName'] + '</td>    <td>' + date.getFullYear() + ' ' + date.getMonth() + ' ' + date.getDate() + '</td>   <td>' + data[i]['comment'] + '</td></tr>')
                 }
             })
     });
 
+    $("#processButton").click(function () {
+        var message = "Id Name                  Country\n";
+        //Loop through all checked CheckBoxes in GridView.
+        $("#names input[type=checkbox]:checked").each(function () {
+            var row = $(this).closest("tr")[0];
+            message += row.cells[1].innerHTML;
+            message += "   " + row.cells[2].innerHTML;
+            message += "   " + row.cells[3].innerHTML;
+            message += "   " + row.cells[4].innerHTML;
+            message += "\n";
+        });
+        alert(message);
+        return false;
+    });
 
 
     // $.get("http://127.0.0.1:8080/getAll")
