8af0d62dbcfbcb2ec503494ca64e89d7156567e0
anderson178 <denismironenko@yandex.ru>
2019-05-26 11:54:08 +0300

    front-end - button fill table

--- src/main/resources/static/js/my.js
+++ src/main/resources/static/js/my.js
@@ -2,19 +2,28 @@ $(document).ready(function () {
 
     // $.get( "http://127.0.0.1:8080/map/getById?parm={parm}", { name: "John", time: "2pm" } );
 
-    $("#button").click(function () {
-        $.get("http://127.0.0.1:8080/getById", {parm: 1})
-            .done(function (data) {
-                // alert( "Data Loaded: " + data );
-                // var obj = jQuery.parseJSON(data);
-                // alert(obj['last_name']);
-                alert(data['lastName'])
-            })
-            .fail(function () {
-                alert("error");
-            });
-    });
+    // $("#button").click(function () {
+    //     $.get("http://127.0.0.1:8080/getById", {parm: 1})
+    //         .done(function (data) {
+    //             // alert( "Data Loaded: " + data );
+    //             // var obj = jQuery.parseJSON(data);
+    //             // alert(obj['last_name']);
+    //             alert(data['lastName'])
+    //         })
+    //         .fail(function () {
+    //             alert("error");
+    //         });
+    // });
 
+    $("#button").click(function () {
+        $.get("http://127.0.0.1:8080/getAll")
+            .done(function (data) {
+                $('#names tbody').empty();
+                for (var i = 0; i < data.length; i++) {
+                    $('#names tbody').append('<tr><td>' + data[i]['id'] + '</td><td>' + data[i]['firstName'] + '</td><td>' + data[i]['lastName'] + '</td><td>' + data[i]['middleName'] + '</td><td>' + data[i]['birthDate'] + '</td><td>' + data[i]['comment'] + '</td></tr>')
+                }
+            })
+    });
 
 
 
